b9dcf1c55ca174aa10ce1448ad9d56b1de92b593
ruko19 <rukobaruna23@gmail.com>
2023-01-14 10:21:36 -0500

    ?

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
 
             <nav className="navbar navbar-expand-lg navbar-light container  ">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#"><img src={IMGLogo} alt="" /></a>
+                    <span className="navbar-brand" ><img src={IMGLogo} alt="" /></span>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
